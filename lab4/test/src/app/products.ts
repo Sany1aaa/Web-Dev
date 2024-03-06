41f8d70d10b087b687f7b98c382f581a671acbc6
Сания <saniya@MacBook-Pro-Sania.local>
2024-03-06 16:05:28 +0500

    haha

---
+++ lab4/test/src/app/products.ts
@@ -0,0 +1,102 @@
+export interface Product {
+  id: number;
+  name: string;
+  price: number;
+  description: string;
+  image: string;
+  rating: number;
+  kaspiLink: string;
+}
+
+export const products = [
+  {
+    id: 1,
+    name: 'Rastar 1:14 70710B Porshe 918 Spyder',
+    price: 29000,
+    description: 'Porsche машинка, 45.0, пульт-управления',
+    image: '/assets/first.jpeg',
+    rating: 4.5,
+    kaspiLink: 'https://kaspi.kz/shop/p/rastar-1-14-70710b-porshe-918-spyder-100745650/?c=750000000&m=30039793&sr=1&qid=83bce968af2acea505eeda9332105d61&ref=shared_link',
+  },
+  {
+    id: 2,
+    name: 'Электромобиль Detskii_avtopark Porshe Cayenne',
+    price: 329984,
+    description: 'автомобиль,черный,нагрузка: 100.0 кг, 3-7лет ',
+    image: '/assets/2.jpeg',
+    rating: 4.8,
+    kaspiLink: 'https://kaspi.kz/shop/p/detskii-avtopark-porshe-cayenne-989418-chernyi-105706858/?c=750000000&sr=1&qid=83bce968af2acea505eeda9332105d61&ref=shared_link',
+  },
+  {
+    id: 3,
+    name: 'Double Horse Porshe 911',
+    price: 4930,
+    description: 'легковая, 1:32, инерционная, зеленый',
+    image: '/assets/third.jpeg',
+    rating: 3.2,
+    kaspiLink: 'https://kaspi.kz/shop/p/double-horse-porshe-911-zelenyi-104248314/?c=750000000&sr=2&qid=83bce968af2acea505eeda9332105d61&ref=shared_link',
+  },
+  {
+    id: 4,
+    name: 'Double Horse Porshe 911 ',
+    price: 4929,
+    description: 'легковая, 1:32, инерционная, розовый',
+    image: '/assets/four.jpeg',
+    rating: 3.3,
+    kaspiLink: 'https://kaspi.kz/shop/p/double-horse-porshe-911-rozovyi-104248209/?c=750000000&sr=4&qid=83bce968af2acea505eeda9332105d61&ref=shared_link',
+  },
+  {
+    id: 5,
+    name: 'Porshe 911 Turbo S ',
+    price: 7650,
+    description: 'легковая, 1:32, инерционная,металл,черный',
+    image: '/assets/five.jpeg',
+    rating: 4.2,
+    kaspiLink: 'https://kaspi.kz/shop/p/porshe-911-turbo-s-12823038-10306-metall-chernyi-115443527/?c=750000000&sr=8&qid=83bce968af2acea505eeda9332105d61&ref=shared_link',
+  },
+  {
+    id: 6,
+    name: 'Porshe 911 Turbo S 12823038_10305 ',
+    price: 4999,
+    description: 'легковая, 1:32, инерционная,металл,белый',
+    image: '/assets/six.jpeg',
+    rating: 4.9,
+    kaspiLink: 'https://kaspi.kz/shop/p/porshe-911-turbo-s-12823038-10305-metall-belyi-115443371/?c=750000000&sr=9&qid=83bce968af2acea505eeda9332105d61&ref=shared_link',
+  },
+  {
+    id: 7,
+    name: 'Porshe 911 Sport 887489',
+    price: 359000,
+    description: 'легковая, 1:24, инерционная,металл,белый',
+    image: '/assets/seven.jpeg',
+    rating: 3.9,
+    kaspiLink: 'https://kaspi.kz/shop/p/porshe-911-sport-887489-metall-belyi-108838528/?c=750000000&sr=14&qid=83bce968af2acea505eeda9332105d61&ref=shared_link',
+  },
+  {
+    id: 8,
+    name: 'YZ Model Porshe 911 ',
+    price: 80600,
+    description: 'легковая,1:36,инерционная,11*5*4,желтый',
+    image: '/assets/8.jpeg',
+    rating: 4.4,
+    kaspiLink: 'https://kaspi.kz/shop/p/yz-model-porshe-911-yellow-ag1314-zheltyi-108223338/?c=750000000&sr=15&qid=83bce968af2acea505eeda9332105d61&ref=shared_link',
+  },
+  {
+    id: 9,
+    name: 'Double Horse Porshe 911',
+    price: 4930,
+    description: 'легковая,1:32,инерционная,серый',
+    image: '/assets/9.jpeg',
+    rating: 4.0,
+    kaspiLink: 'https://kaspi.kz/shop/p/double-horse-porshe-911-seryi-104248225/?c=750000000&sr=19&qid=83bce968af2acea505eeda9332105d61&ref=shared_link',
+  },
+  {
+    id: 10,
+    name: 'Электромобиль Porshe 298666 ',
+    price: 61807,
+    description: 'тип: автомобиль,скорость: 6.0 км/ч, 1 год',
+    image: '/assets/10.jpeg',
+    rating: 4.9,
+    kaspiLink: 'https://kaspi.kz/shop/p/porshe-298666-krasnyi-112639771/?c=750000000&sr=13&qid=83bce968af2acea505eeda9332105d61&ref=shared_link',
+  }
+];
